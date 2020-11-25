5185f162f0b9790ce5442e95e03c8d67a6776721
Bill Mårtensson <bill@magictechnology.se>
2020-11-25 12:49:25 +0100

    Initial Commit

---
+++ Klockapp/KlockappApp.swift
@@ -0,0 +1,17 @@
+//
+//  KlockappApp.swift
+//  Klockapp
+//
+//  Created by Bill Martensson on 2020-11-25.
+//
+
+import SwiftUI
+
+@main
+struct KlockappApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
